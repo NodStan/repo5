e3c33560289086fbeccc7273e882dae6375b6e57
Stanley <stanleyobitunwase@gmail.com>
2024-04-12 13:55:56 +0100

    Hospital

---
+++ main.java
@@ -0,0 +1,19 @@
+package Hospital;
+
+public class main {
+    public static void main(String[] args) {
+//        ward ward1 = new ward();
+//        ward1.setBed("rocky-bed");
+//        ward1.getBed();
+//
+//        Patient patient = new Patient();
+//        patient.name = "Michael";
+//        patient.injury = "Mental issues";
+//        Doctor doctor = new Doctor();
+//        doctor.setDName("Stanley");
+//        System.out.println(patient.name + " your doctor is " + doctor.name);
+        Dog dog = new Dog("Bingo");
+        Dog dog1 = new Dog("Bingo", "Bulldog");
+        Dog dog2 = new Dog("Bingo", "Bulldog", 10);
+    }
+}
