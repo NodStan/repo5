581e7f9bc7f1b79126cc31c2a9567c494b714a8a
Stanley <stanleyobitunwase@gmail.com>
2024-04-12 13:52:30 +0100

    Hospital

---
+++ Doctor.java
@@ -0,0 +1,15 @@
+package Hospital;
+
+public class Doctor {
+    String name;
+    private String gender;
+    private String degree;
+
+    public void setDName(String name) {
+        this.name = name;
+    }
+
+    public String getname(){
+        return name;
+    }
+}
